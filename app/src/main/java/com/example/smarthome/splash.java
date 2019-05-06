e11ff6b08ab757152ea727df4777bd5fcf8865ef
Ayu Rahmawati <ayurahmawatii@users.noreply.github.com>
2019-05-06 20:12:39 +0700

    Update splash.java

--- app/src/main/java/com/example/smarthome/splash.java
+++ app/src/main/java/com/example/smarthome/splash.java
@@ -1,7 +1,9 @@
 package com.example.smarthome;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class splash extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    public void bukaDashboard(View view) {
+        Intent dash = new Intent(this, cardview.class);
+        startActivity(dash);
     }
 }
