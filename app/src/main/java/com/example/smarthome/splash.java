a63561e2fdf6638ba5fd681289dbff07d8e19af3
mragil <mragill98@gmail.com>
2019-05-10 21:19:43 +0700

    Animasi Splash Screen

--- app/src/main/java/com/example/smarthome/splash.java
+++ app/src/main/java/com/example/smarthome/splash.java
@@ -4,13 +4,21 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class splash extends AppCompatActivity {
-
+    TextView tagline;
+    Animation bottom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        tagline = findViewById(R.id.tagline_app);
+        bottom = AnimationUtils.loadAnimation(this,R.anim.bottom);
+        tagline.setAnimation(bottom);
 
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
