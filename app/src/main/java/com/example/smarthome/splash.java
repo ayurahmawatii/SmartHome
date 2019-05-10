47eacf2dfc601172bd77f75b1f4fc099322501cc
mragil <mragill98@gmail.com>
2019-05-10 20:39:09 +0700

    Aktifin Splash screen nya

--- app/src/main/java/com/example/smarthome/splash.java
+++ app/src/main/java/com/example/smarthome/splash.java
@@ -1,9 +1,9 @@
 package com.example.smarthome;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class splash extends AppCompatActivity {
 
@@ -11,10 +11,14 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-    }
 
-    public void bukaDashboard(View view) {
-        Intent dash = new Intent(this, cardview.class);
-        startActivity(dash);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000L); //3000 L = 3 detik
     }
 }
