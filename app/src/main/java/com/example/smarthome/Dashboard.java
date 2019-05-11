9d88ef1196c610872f5325ed5e629aea5a44c42a
mragil <mragill98@gmail.com>
2019-05-11 12:37:31 +0700

    Harus Verifikasi Email Dulu Buat Lanjut APP Nya

--- app/src/main/java/com/example/smarthome/Dashboard.java
+++ app/src/main/java/com/example/smarthome/Dashboard.java
@@ -9,14 +9,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Dashboard extends AppCompatActivity {
     FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthListner;
-    TextView emailText,nameText;
+    TextView emailText,verifyText;
 
     @Override
     protected void onStart() {
@@ -26,29 +29,50 @@ public class Dashboard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        Button button = (Button) findViewById(R.id.signout);
+        Button verifbtn = findViewById(R.id.verify_btn);
+        mAuth = FirebaseAuth.getInstance();
         emailText = findViewById(R.id.email_user);
-        nameText = findViewById(R.id.name_user);
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        verifyText = (TextView) findViewById(R.id.verify_user);
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             // Name, email address, and profile photo Url
-            String name = user.getDisplayName();
             String email = user.getEmail();
-            Uri photoUrl = user.getPhotoUrl();
+            emailText.setText(email);
             Log.d("EMAIL USER",email);
 
 
             // Check if user's email is verified
             boolean emailVerified = user.isEmailVerified();
+            if(emailVerified){
+                Log.d("VERIF?","UDAH");
+                verifyText.setText("Verified Account");
+                verifbtn.setVisibility(View.INVISIBLE);
+
+            }else{
+                Log.d("VERIF?","BELUM");
+                verifyText.setText("Please Verify Your Account");
+                verifbtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(Dashboard.this, "Verification Email Sent", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+            }
 
             // The user's ID, unique to the Firebase project. Do NOT use this value to
             // authenticate with your backend server, if you have one. Use
             // FirebaseUser.getIdToken() instead.
             String uid = user.getUid();
         }
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-        Button button = (Button) findViewById(R.id.signout);
-        mAuth = FirebaseAuth.getInstance();
+
         mAuthListner = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -64,5 +88,6 @@ public class Dashboard extends AppCompatActivity {
                 mAuth.signOut();
             }
         });
+
     }
 }
