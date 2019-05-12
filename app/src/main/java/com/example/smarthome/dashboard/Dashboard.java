a90cb6683ca697bd55dfb8b08999b7709540caca
Ayu Rahmawati <ayurahmawatii@users.noreply.github.com>
2019-05-12 22:32:21 +0700

    penambahan option menu

--- app/src/main/java/com/example/smarthome/dashboard/Dashboard.java
+++ app/src/main/java/com/example/smarthome/dashboard/Dashboard.java
@@ -1,12 +1,18 @@
 package com.example.smarthome.dashboard;
 
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.smarthome.LoginActivity;
 import com.example.smarthome.R;
+import com.example.smarthome.Register;
+import com.example.smarthome.mode;
 
 import java.util.ArrayList;
 
@@ -20,6 +26,14 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        TextView setting = (TextView) findViewById(R.id.login_page);
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Dashboard.this, mode.class);
+                startActivity(intent);
+
         dashboardModelArrayList = new ArrayList<>();
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview1);
         initializeData();
