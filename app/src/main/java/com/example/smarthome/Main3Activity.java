e2907ea4a81899d69cdc5f321f778d0463e4ccea
mragil <mragill98@gmail.com>
2019-05-12 21:32:48 +0700

    Suhuuuu

--- app/src/main/java/com/example/smarthome/Main3Activity.java
+++ app/src/main/java/com/example/smarthome/Main3Activity.java
@@ -1,19 +1,45 @@
 package com.example.smarthome;
 
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class Main3Activity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+public class Main3Activity extends AppCompatActivity {
+    DatabaseReference suhuRuangan ;
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-
-        TextView t = findViewById(R.id.suhu);
-        Typeface myCustomFont=Typeface.createFromAsset(getAssets(),"fonts/fofbb_ital.ttf");
+        t = findViewById(R.id.suhu);
+        Typeface myCustomFont=Typeface.createFromAsset(getAssets(),"fonts/Springwood.otf");
         t.setTypeface(myCustomFont);
+
+        suhuRuangan = FirebaseDatabase.getInstance().getReference().child("suhuRuangan");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        suhuRuangan.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String suhu = dataSnapshot.getValue(String.class)+"°"+"C";
+                t.setText(suhu);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
