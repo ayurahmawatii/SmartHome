1d526a4242b2fbe9901001920d629bd3f5b12c30
Ayu Rahmawati <ayurahmawatii@users.noreply.github.com>
2019-05-13 01:16:54 +0700

    update font about

--- app/src/main/java/com/example/smarthome/about.java
+++ app/src/main/java/com/example/smarthome/about.java
@@ -17,5 +17,16 @@ public class about extends AppCompatActivity {
         Typeface myCustomFont=Typeface.createFromAsset(getAssets(),"fonts/Rustic.otf");
         t.setTypeface(myCustomFont);
 
+        t = (TextView) findViewById(R.id.about1);
+        Typeface myCustomFont1=Typeface.createFromAsset(getAssets(),"fonts/Rustic.otf");
+        t.setTypeface(myCustomFont1);
+
+        t = (TextView) findViewById(R.id.about2);
+        Typeface myCustomFont2=Typeface.createFromAsset(getAssets(),"fonts/Rustic.otf");
+        t.setTypeface(myCustomFont2);
+
+        t = (TextView) findViewById(R.id.about3);
+        Typeface myCustomFont3=Typeface.createFromAsset(getAssets(),"fonts/Rustic.otf");
+        t.setTypeface(myCustomFont3);
     }
 }
